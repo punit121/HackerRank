cc95204d24576fafbcf5b2371b6f785350e1cd8b
punit121 <punit.kumar1661@gmail.com>
2017-07-11 00:15:00 +0530

    insertionsort corrected

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,27 +1,38 @@
 #include<stdio.h>
 
 void insertionsort(int a[],int n){
-int i,j;
-
+int i,j,key;
+for(j=1;j<n;j++){
+    key=a[j];
+    i=j-1;
+    while(i>=0&&key < a[i]){
+            a[i+1]=a[i];
+            i--;
+    }
+    a[i+1]=key;
+}
 
 
 
 }
-void printList(int a[],int n){
-
+void printlist(int a[],int n){
+int i;
+for(i=0;i<n;i++){
+    printf("%d  ",a[i]);
+}
 }
 int main()
 {
 int n,a[1000],i,j;
-printf("Enter total no. of no.s to keep in array")
+printf("Enter total no. of no.s to keep in array ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
     scanf("%d",&a[i]);
 }
-insertionsort(a);
+insertionsort(a,n);
 
-printList(a)
+printlist(a,n);
 
 
 }
