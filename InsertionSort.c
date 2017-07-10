4f15c9b48da8c9ce7d5167a5f0230425cbedc650
punit121 <punit.kumar1661@gmail.com>
2017-07-10 23:54:41 +0530

    insertionsort added

---
+++ InsertionSort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void insertionsort(int a[],int n){
+int i,j;
+
+
+
+
+}
+void printList(int a[],int n){
+
+}
+int main()
+{
+int n,a[1000],i,j;
+printf("Enter total no. of no.s to keep in array")
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+insertionsort(a);
+
+printList(a)
+
+
+}
