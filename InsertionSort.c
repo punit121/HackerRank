f1422a57082342bb563ec582e593e23282ae6b27
punit121 <punit.kumar1661@gmail.com>
2017-07-23 15:37:51 +0530

    code added

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,38 +1,37 @@
 #include<stdio.h>
 
-void insertionsort(int a[],int n){
-int i,j,key;
-for(j=1;j<n;j++){
-    key=a[j];
-    i=j-1;
-    while(i>=0&&key < a[i]){
+void insertion_sort(int a[],int n){
+    int i,j,k;
+    for(j=1;j<n;j++){
+        k=a[j];
+        i=j-1;
+        while(i>=0 && a[i]>k){
             a[i+1]=a[i];
             i--;
+
+        }
+        a[i+1]=k;
+
     }
-    a[i+1]=key;
-}
-
-
 
 }
-void printlist(int a[],int n){
-int i;
-for(i=0;i<n;i++){
-    printf("%d  ",a[i]);
-}
-}
+
 int main()
 {
-int n,a[1000],i,j;
-printf("Enter total no. of no.s to keep in array ");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-    scanf("%d",&a[i]);
+    //printf("Hello, World!\n Enter N : ");
+    int a[100],i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+
+    }
+    insertion_sort(a,n);
+
+    for(i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+
+
+    return 0;
 }
-insertionsort(a,n);
 
-printlist(a,n);
-
-
-}
